362f76c3e2e6ed8fa51f618487dabb081504a362
sanjeevblr <sanjeev.singh@traveloka.com>
2019-03-10 15:35:30 +0700

    flight-inventory added

---
+++ flight-inventory/src/main/java/com/traveloka/tdd/workshop/flightinventory/FlightInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.traveloka.tdd.workshop.flightinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightInventoryApplication.class, args);
+    }
+
+}
---
+++ flight-inventory/src/test/java/com/traveloka/tdd/workshop/flightinventory/FlightInventoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.traveloka.tdd.workshop.flightinventory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightInventoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
